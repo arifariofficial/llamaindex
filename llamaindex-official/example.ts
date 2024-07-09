30bdbd4b1d1706bd06678d810472f323b4dc38b7
Ariful Islam <ariful6@yahoo.com>
2024-07-09 03:11:54 +0300

    example.ts finish

--- llamaindex-official/example.ts
+++ llamaindex-official/example.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import fs from "node:fs/promises";
 import { Document, MetadataMode, NodeWithScore, VectorStoreIndex } from "llamaindex";
 
@@ -6,4 +9,28 @@ async function main() {
   const path = "node_modules/llamaindex/examples/abramov.txt";
 
   const essay = await fs.readFile(path, "utf-8");
+
+  // Create Document object with the essay
+  const document = new Document({ text: essay, id_: path });
+
+  // Split text and create embeddings. Store them in a VectorStoreIndex
+  const index = await VectorStoreIndex.fromDocuments([document]);
+
+  // Query the index
+  const queryEngine = index.asQueryEngine();
+  const { response, sourceNodes } = await queryEngine.query({
+    query: "What did author do in college?",
+  });
+
+  if (sourceNodes) {
+    sourceNodes.forEach((source: NodeWithScore, index: number) => {
+      console.log(
+        `\n${index}: Score: ${source.score} - ${source.node
+          .getContent(MetadataMode.NONE)
+          .substring(0, 50)}...\n`
+      );
+    });
+  }
 }
+
+main().catch(console.error);
